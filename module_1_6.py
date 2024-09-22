5e6dcf07f3afffa76fd222c7cf1edc66107cd053
Olya-Leb <olya.g.swan@gmail.com>
2024-09-22 23:20:40 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,17 @@
+from operator import truediv
+
+my_dict = {'Olya' : 2001, 'Vitaly' : 2000, 'Polina' : 1962, 'Sveta' : 1999}
+print(my_dict)
+print(my_dict['Olya'])
+print(my_dict.get('Max','Не существует'))
+my_dict['Alena'] = 1999
+my_dict['Den'] = 1999
+print(my_dict)
+my_dict.pop('Alena')
+print(my_dict)
+my_set = {1,1,2,2,'str','str',4,4,False,True,False,True}
+print(my_set)
+my_set.add('Sasha'), my_set.add('Masha')
+print(my_set)
+my_set.discard(False)
+print(my_set)
