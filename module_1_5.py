1b42a473303a717a962744b0cc0f3f237c4055c1
Olya <olya.g.swan@gmail.com>
2024-09-25 11:03:02 +0500

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -2,7 +2,7 @@ immutable_var = (0,'str',True)
 print(immutable_var)
 #immutable_var[0] = 5 #т.к. "tuple" являеться неизменяемым типом данных,
                       то соответсвенно мы не можем изменить значение 
-                       какого-либо элемента после создания переменной типа tuple.
+                     какого-либо элемента после создания переменной типа tuple.
 
 mutable_list = [1,"string",False]
 print(mutable_list)
