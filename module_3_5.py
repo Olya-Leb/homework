4e4f37110a970e51c53cc948f1f999a57ec8e3d1
Olya <olya.g.swan@gmail.com>
2024-10-09 16:58:24 +0500

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,9 +1,13 @@
 def get_multiplied_digits(number):
+    number = int(number)
     str_number = str(number)
-    new_str_number = str_number.rstrip('0')
-    first = int(new_str_number[0])
-    if len(new_str_number) > 1:
-        return first * get_multiplied_digits(int(new_str_number[1:]))
+    first = int(str_number[0])
+
+    if str_number.endswith('0'):
+        str_number = str_number.rstrip('0')
+
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
@@ -11,3 +15,5 @@ result1 = get_multiplied_digits(402030)
 print(result1)
 result2 = get_multiplied_digits(40203)
 print(result2)
+result3 = get_multiplied_digits(0)
+print(result3)
