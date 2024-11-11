7d63d2e0611d79e18ea9eb589f982d856a7914d9
Olya <olya.g.swan@gmail.com>
2024-11-11 22:27:19 +0500

    Update module_7_1.py

--- module_7_1/module_7_1.py
+++ module_7_1/module_7_1.py
@@ -1,32 +1,44 @@
-from pprint import pprint
+from pprint import pprint # импорт функции "pretty-print", из модуля "pretty-print"
 
 class Product:
+    """Класс описывающий Продукт"""
     def __init__(self, name, weight, category):
         self.name = str(name) # название продукта (строка)
         self.weight = float(weight) # общий вес товара (дробное число) (5.4, 52.8 и т.п.)
         self.category = str(category) # категория товара (строка)
 
-    def __str__(self): # возвращает строку в формате '<название>, <вес>, <категория>
+    def __str__(self):
+        """Метод, который возвращает строку в формате '<название>, <вес>, <категория>'.
+        Все данные в строке разделены запятой с пробелами."""
         return f'{self.name}, {self.weight}, {self.category}'
 
 class Shop:
-    __file_name = 'products.txt'
+    """Класс описывающий Магазин"""
+
+    __file_name = 'products.txt' # Инкапсулированный атрибут
     def get_products(self):
-        file = open(self.__file_name, 'r')
-        file_contents = file.read()
-        file.close()
+        """Метод, который считывает всю информацию из файла __file_name,
+        закрывает его и возвращает единую строку со всеми товарами из файла __file_name."""
+
+        file = open(self.__file_name, 'r') # открытие файла в режиме чтения и присваивание переменной "file"
+        file_contents = file.read() # присваивание содержимого файла в переменную "file_contents"
+        file.close() # закрытие файла
         # pprint(r)
-        return file_contents
+        return file_contents # возвращает переменную "file_contents"
 
     def add(self, *products):
+        """Метод, который принимает неограниченное количество объектов класса Product.
+        Добавляет в файл __file_name каждый продукт из products, если его ещё нет в файле (по названию).
+        Если такой продукт уже есть, то не добавляет и выводит строку 'Продукт <название> уже есть в магазине' """
+
         file_get = self.get_products()
         for i in products:
-            if file_get.find(f'{i.name}') == -1:
-                file = open(self.__file_name, 'a')
-                file.write(f'{i}\n')
-                file.close()
-            else:
-                print(f'Продукт {i} уже есть в магазине')
+                if file_get.find(f'{i.name}') == -1:
+                    file = open(self.__file_name, 'a')
+                    file.write(f'{i}\n')
+                    file.close()
+                else:
+                    print(f'Продукт {i} уже есть в магазине')
 
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
