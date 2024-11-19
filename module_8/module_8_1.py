d7fe08179df07840e28d10a7c8fcc3b1c383c08c
Olya <olya.g.swan@gmail.com>
2024-11-19 19:55:50 +0500

    Update module_8_1.py

--- module_8/module_8_1.py
+++ module_8/module_8_1.py
@@ -8,8 +8,6 @@ def add_everything_up(a, b):
         result2 = f'{a}{b}'
         return result2
 
-print(add_everything_up(5, '5'))
-print(add_everything_up('5', 0))
-print(add_everything_up('string', 5.5))
-print(add_everything_up(5.5, '5'))
-print(add_everything_up('5.5', '5'))
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
