329762aef54a6461dd86524496fdb72935297f63
Olya <olya.g.swan@gmail.com>
2025-01-11 13:34:30 +0500

    Update tests_12_3.py

--- module_12/tests_12_3_and_suite_12_3/tests_12_3.py
+++ module_12/tests_12_3_and_suite_12_3/tests_12_3.py
@@ -6,18 +6,18 @@ class RunnerTest(unittest.TestCase):
     @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
     def test_walk(self):
         """Test for walk function in runner"""
-        runner1 = Runner("Artur")
+        runner = Runner("Artur")
         for _ in range(10):
-            runner1.walk()
-        self.assertEqual(runner1.distance, 50)
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
 
     @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
     def test_run(self):
         """Test for run function in runner"""
-        runner2 = Runner("Maria")
+        runner = Runner("Maria")
         for _ in range(10):
-            runner2.run()
-        self.assertEqual(runner2.distance, 100)
+            runner.run()
+        self.assertEqual(runner.distance, 100)
 
     @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
     def test_challenge(self):
