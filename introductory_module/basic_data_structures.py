75845a74afb654bc0df0dee4c75c8f225e884003
Olya <olya.g.swan@gmail.com>
2025-01-13 16:28:55 +0500

    Update and rename basic_data_structures14.09.2024.py to basic_data_structures.py

---
+++ introductory_module/basic_data_structures.py
@@ -0,0 +1,15 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3nd program
+print(a := 2 * 2 + 2)
+print(b := 2 * (2 + 2))
+print(a == b)
+
+# 4th program
+print(result := float("123.456") * 10)
+print(result := result % 10)
+print(int(result))
