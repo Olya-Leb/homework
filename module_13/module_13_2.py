064f541359faedf9bac9f76fd410cfcfb0b9465c
Olya <olya.g.swan@gmail.com>
2025-01-13 22:31:27 +0500

    Add files via upload

---
+++ module_13/module_13_2.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "943289834:AAHmOxwkyl8"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=["start"])
+async def start(message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_massages(message):
+    print("Введите команду /start, чтобы начать общение.")
+
+def main():
+    executor.start_polling(dp, skip_updates=True)
+
+if __name__ == "__main__":
+    main()
